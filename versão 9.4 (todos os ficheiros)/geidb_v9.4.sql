0029affd9a7fea2de75037e9001f59b347abf129
migper-gei <82807068+migper-gei@users.noreply.github.com>
2023-02-10 19:48:22 +0000

    v9.5

--- versão 9.4 (todos os ficheiros)/geidb_v9.4.sql
+++ versão 9.4 (todos os ficheiros)/geidb_v9.4.sql
@@ -335,6 +335,35 @@ ALTER TABLE `tipos_manutencao`
   ADD PRIMARY KEY (`id`);
 
 
+
+
+CREATE TABLE `settings` (
+  `id` int(11) NOT NULL,
+  `email_user` varchar(255) NOT NULL,
+  `pass` blob NOT NULL,
+  `email_smtp` varchar(255) NOT NULL,
+  `email_smtpport` int(11) NOT NULL,
+  `nome_app` varchar(255) NOT NULL,
+  `sessao_timeout` int(11) NOT NULL
+) ENGINE=InnoDB DEFAULT CHARSET=utf8;
+
+
+ALTER TABLE `settings`
+  ADD PRIMARY KEY (`id`) USING BTREE;
+
+
+ALTER TABLE `settings`
+  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT, AUTO_INCREMENT=4;
+COMMIT;
+
+
+
+
+
+
+
+
+
 --
 -- AUTO_INCREMENT de tabela `tipos_manutencao`
 --
