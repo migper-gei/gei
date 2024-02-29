711a32f3c5788897c0e159eb2e7d118b5984a873
migper-gei <82807068+migper-gei@users.noreply.github.com>
2024-02-29 14:58:49 +0000

    Update footer.php

--- versão 11 (todos os ficheiros)/footer.php
+++ versão 11 (todos os ficheiros)/footer.php
@@ -12,7 +12,7 @@
   width: 100%; /* Full width */
   height: 100%; /* Full height */
   overflow: auto; /* Enable scroll if needed */
-  background-color: rgb(0,0,0); /* Fallback color */
+  background-color: #000; /*rgb(0,0,0);  Fallback color */
   background-color: rgba(0,0,0,0.4); /* Black w/ opacity */
 }
 
@@ -29,7 +29,7 @@
 
 /* The Close Button */
 .close {
-  color: #aaaaaa;
+  color: #aaa;
   float: right;
   font-size: 28px;
   font-weight: bold;
