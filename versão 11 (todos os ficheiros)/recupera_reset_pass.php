de892a3a8796c1d0dad95b39fbfdfd32333fbd76
migper-gei <82807068+migper-gei@users.noreply.github.com>
2024-04-15 16:17:31 +0100

    V11

--- versão 11 (todos os ficheiros)/recupera_reset_pass.php
+++ versão 11 (todos os ficheiros)/recupera_reset_pass.php
@@ -3,6 +3,28 @@
    <head>
 
 
+   <script>
+function email_validation(){
+'use strict';
+
+var mailformat = /^\w+([\.\-]?\w+)*@\w+([\.\-]?\w+)*(\.\w{2,3})+$/;
+var email_name = document.getElementById("email");
+var email_value = document.getElementById("email").value;
+var email_length = email_value.length;
+if(!email_value.match(mailformat) || email_length === 0)
+{
+
+document.getElementById('email_err').innerHTML = '<br>'+'Email inválido.';
+email_name.focus();
+document.getElementById('email_err').style.color = "#FF0000";
+}
+else
+{
+//document.getElementById('email_err').innerHTML = 'Valid email format';
+//document.getElementById('email_err').style.color = "#00AF33";
+}
+}
+</script>
 
 <?php include ("head.php");
 
@@ -136,7 +158,8 @@ if ($rsenha==0)
     <form action = "<?php echo SVRURL ?>recupera_pass_OK.php" method = "post">
 
       <input  title="Email"  required pattern="[a-z0-9._%+-]+@[a-z0-9.-]+\.[a-z]{2,63}$"
-					type = "text" name = "email"  class="fadeIn second"  placeholder="Email">
+					type = "text" name = "email"  class="fadeIn second"  placeholder="Email"
+               onBlur="email_validation();" id="email"><span id="email_err"></span>
  
       <input title="Recuperar (enviar email)"  type="submit" class="fadeIn fourth" value="Recuperar">
   
