101c8ddf37b8c0f5fb6de29c4166e5421bc75b1a
migper-gei <migarper@gmail.com>
2025-05-26 14:59:30 +0100

    Update config_serverbd_settings.php

--- Versão 16 (todos os ficheiros)/config_serverbd_settings.php
+++ Versão 16 (todos os ficheiros)/config_serverbd_settings.php
@@ -2,9 +2,9 @@
 <?php
 
 
-define('DB_SERVER', 'localhost:3306');
+define('DB_SERVER', 'localhost');
 define('DB_USERNAME', 'root');
-define('DB_PASSWORD', 'fm1003+-FM');
+define('DB_PASSWORD', '-----');
 define('DB_DATABASE', 'gei_escolas_instituicoes');
   
 
