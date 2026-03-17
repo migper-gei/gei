322769f067160a45e0d995da96e2f3168a8f72a8
migper-gei <migarper@gmail.com>
2026-03-17 21:41:53 +0000

    Delete versão 9.0 directory

--- versão 9.0/dados_enviar_email.php
+++
@@ -1,7 +0,0 @@
-        <?php
-        $mail->IsHTML(true);
-        $mail->From="geimasterdb@hotmail.com";
-        $mail->FromName="GEI";
-        $mail->Sender="geimasterdb@hotmail.com";
-        $mail->AddReplyTo('geimasterdb@hotmail.com','GEI');
-        ?>
